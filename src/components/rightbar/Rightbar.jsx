1547462f9ee51ceb45791a717cfbbaf358b76caa
Sotham <lanches.thomas@gmail.com>
2023-02-25 11:17:23 +0100

    work on responsive rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -11,10 +11,10 @@ import {
     ListItemText,
     Typography,
 } from '@mui/material'
-import React from 'react'
 import { styled } from '@mui/material/styles'
 import postData from '../../data/postData.json'
 import { useTheme } from '@material-ui/core/styles'
+import React, { useState, useEffect } from 'react'
 
 const StyledBox = styled(Box)(({ theme }) => ({
     border: `1px solid ${theme.palette.primary.light}`,
@@ -34,7 +34,6 @@ const StyledAvatar = styled(Avatar)`
     overflow: visible;
     border-radius: 20px;
 
-
     & img {
         border-radius: 50%;
     }
@@ -48,7 +47,7 @@ const StyledAvatar = styled(Avatar)`
         font-size: 11px;
         background-color: ${(props) => props.theme.palette.common.white};
         color: ${(props) => props.theme.palette.common.black};
-        text-align:center;
+        text-align: center;
     }
 
     &:hover {
@@ -60,20 +59,48 @@ const StyledAvatar = styled(Avatar)`
 
 const Rightbar = () => {
     const theme = useTheme()
+    const [maxAvatars, setMaxAvatars] = useState(7)
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth < 600) {
+                setMaxAvatars(1)
+            } else if (window.innerWidth < 960) {
+                setMaxAvatars(3)
+            } else if (window.innerWidth < 1280) {
+                setMaxAvatars(5)
+            } else {
+                setMaxAvatars(7)
+            }
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
         <StyledBox
-            p={4}
+            px={4}
             sx={{ display: { xs: 'none', sm: 'flex' } }}
             justifyContent={'center'}
-            width='20%'
-            
+            width="20%"
         >
             <StyledBoxContent>
-                <Box width="100%" py={2} display='flex' flexDirection='column' justifyContent='space-around' gap={2} sx={{border:`2px solid ${theme.palette.background.default}`}}>
-                    <Typography variant="h6" fontWeight={100} sx={{border:'2px solid red'}}>
+                <Box
+                    width="100%"
+                    py={2}
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent="space-around"
+                    gap={2}
+                >
+                    <Typography variant="h6" fontWeight={100}>
                         Online Friends
                     </Typography>
-                    <AvatarGroup max={7} sx={{ overflow: 'visible', border:'2px solid green' }}>
+                    <AvatarGroup max={maxAvatars} sx={{ overflow: 'visible' }}>
                         {postData.map((data) => (
                             <StyledAvatar
                                 alt={`${data.name}`}
@@ -86,49 +113,16 @@ const Rightbar = () => {
                     </AvatarGroup>
                 </Box>
                 <Typography>Latest Photos</Typography>
-                <ImageList cols={3} rowHeight={150}>
-                    <ImageListItem>
-                        <img
-                            src={`https://picsum.photos/300/221`}
-                            alt={'item.title'}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <img
-                            src={`https://picsum.photos/300/223`}
-                            alt={'item.title'}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <img
-                            src={`https://picsum.photos/300/225`}
-                            alt={'item.title'}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <img
-                            src={`https://picsum.photos/300/228`}
-                            alt={'item.title'}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <img
-                            src={`https://picsum.photos/300/229`}
-                            alt={'item.title'}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                    <ImageListItem>
-                        <img
-                            src={`https://picsum.photos/300/230`}
-                            alt={'item.title'}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
+                <ImageList cols={3} ws={2} rowHeight={150}>
+                    {postData.map((data) => (
+                        <ImageListItem >
+                            <img
+                                src={`${data.img}`}
+                                alt={'item.title'}
+                                loading="lazy"
+                            />
+                        </ImageListItem>
+                    ))}
                 </ImageList>
                 <Box display={'flex'} flexDirection={'column'} mt={2}>
                     <Typography variant="h6" fontWeight={100}>
